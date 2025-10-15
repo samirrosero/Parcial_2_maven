ce26f55d27c83dd912f685d018545486ebd37368
samir <samir_armero16@hotmail.com>
2025-10-14 20:56:47 -0500

    creyendo que esta solucionando

--- parcial2/src/main/java/com/parcial2/modelo/Criatura.java
+++ parcial2/src/main/java/com/parcial2/modelo/Criatura.java
@@ -1,23 +1,27 @@
 package com.parcial2.modelo;
 
 public abstract class Criatura {
-protected String nombre;
-protected int salud;
-protected int fuerza;
-protected Arma arma;
+    protected String nombre;
+    protected int salud;
+    protected int fuerza;
+    protected Arma arma;
 
-public Criatura(String nombre, int salud, int fuerza) {
-    this.nombre = nombre;
-    this.salud = salud;
-    this.fuerza = fuerza;
-}
- public abstract void atacar(Criatura objetivo);
- public abstract void defender(int daño);
- public boolean estaVivo() {
-    return salud > 0;
+    public Criatura(String nombre, int salud, int fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fuerza = fuerza;
     }
- public void equiparArma(Arma arma) {
-    this.arma = arma;
- }
+
+    public abstract void atacar(Criatura objetivo);
+
+    public abstract void defender(int daño);
+
+    public boolean estaVivo() {
+        return salud > 0;
+    }
+
+    public void equiparArma(Arma arma) {
+        this.arma = arma;
+    }
 
 }
