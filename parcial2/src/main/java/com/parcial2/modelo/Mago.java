635c639c70183e1985e86b583267018645aa99d2
Valeri-Solis1 <vasolis@estudiante.uniajc.edu.co>
2025-10-15 14:46:05 -0500

    "creando mago"

--- parcial2/src/main/java/com/parcial2/modelo/Mago.java
+++ parcial2/src/main/java/com/parcial2/modelo/Mago.java
@@ -4,7 +4,43 @@ import com.parcial2.interfaces.Magico;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Mago{
+public class Mago extends Criatura implements Magico {
+    private List<String> hechizos = new ArrayList<>();
+
+    public Mago(String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+    }
+
+    @Override
+    public void atacar(Criatura objetivo) {
+        int daño = fuerza; // Los magos hacen más daño con hechizos
+        if (arma != null) daño += arma.getDañoAdicional();
+        System.out.println(nombre + " ataca a " + objetivo.nombre +  " causando " + daño + " de daño.");
+        objetivo.defender(daño);
+    }
+
+    @Override
+    public void defender(int daño) {
+        salud -= daño;
+        System.out.println(nombre + " recibe " + daño + " de daño. Salud restante: " + salud);
+    }
+
+    @Override
+    public void lanzarHechizo(Criatura objetivo) {
+        if (hechizos.isEmpty()) {
+            System.out.println(nombre + " no conoce hechizos aún.");
+        } else {
+            System.out.println(nombre + " lanza " + hechizos.get(0) + "sobre " + objetivo.nombre + " !");
+        }
+    }
+
+    @Override
+    public void aprenderHechizo(String hechizo) {
+        hechizos.add(hechizo);
+        System.out.println(nombre + " ha aprendido el hechizo: " + hechizo);
+    }
+
+
 
 
 
